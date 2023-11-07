3b8efde529d31395806dbfc7f9b7af558fb077a2
owenboy9 <owenkraus9@gmail.com>
2023-11-07 21:41:28 +0100

    RandomWords is a class with a method that picks one random word out of the word list. i hope it really works because i only tested it in test.js which is were i also reformatted word-list.csv (will still need to remove all the  ''

---
+++ random-word.js
@@ -0,0 +1,21 @@
+import { readFileSync } from 'node:fs'
+
+
+let randomWords = readFileSync("data/word-list.csv", "utf8")
+
+randomWords = randomWords.trim().split(",")
+
+export default class RandomWord{
+
+  constructor(randomWord) {
+    this.randomWord = this.getRandomWord()
+  }
+
+  getRandomWord() {
+    
+    let generateIndex = Math.random() * randomWords.length
+    let generateWord = randomWords[Math.floor(generateIndex)]
+    return generateWord
+
+  }
+}
