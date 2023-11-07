5c52babf36a389651e11cc5c020288a4f3a9a862
owenboy9 <owenkraus9@gmail.com>
2023-11-07 21:12:10 +0100

    created a .csv file with an array of 100 words (that i will most likely need to format in a different way?) and a test file as a convenient way to run code outside classes. in it. i converted the original single row of 169 words into the 100-word array that now constitutes the word-list. i don't know how to make the console in vsc show all 169 words in the array, instead of letting me know ...69 more

---
+++ test.js
@@ -0,0 +1,10 @@
+let wordList = "woke feel swimming trying singe added bracing flee know forget shower stuck danced darted wrestled covered glaring twisting heard climbed balanced fainted stumbled tell saw stood occupying enjoying cooled drop welded emerged whispered give prickled boiling scream flayed stayed end spread took pleasure parading corseted smirking claim looked shared snowing chapter muscles limbs stranger lungs chest mole hair face swimmers heels fingertips threshold people locker room boys men eyes saint prayers flipflops goggles haste swim shorts soap smell pool sauna restroom lack towel room creatures vowels breath heart voices eavesdrop time verge disclosure ground willow tree body turn eyelids limbs stream steel reassurance stare hate skin world makeup calm survivor boxers hint dick place tapas corner timelines urge haircut sore asymmetrically beat up vague brave many small athletic silent slowly incoherent clumsy sharp darkened whale belly wide slow heavy lowered defiant eventually awkwardly suddenly fixed ancient only bent hard gradually cold strong imaginary old powerless obvious methodically triumphant corseted greek great packed another horrible tenderly opposite almost somehow asymmetrical"
+
+
+function cleanUp() {
+  let step = wordList.split(" ")
+  return step
+
+}
+
+console.log(cleanUp())
