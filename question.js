2a3ee018347779327f6378dd3c156cce99a4b776
owenboy9 <owenkraus9@gmail.com>
2023-11-07 19:30:26 +0100

    question class import prompt sync enables question & harvests user's answer method preventing user from typing in anything but letter

---
+++ question.js
@@ -0,0 +1,28 @@
+import Module from "node:module";
+const require = Module.createRequire(import.meta.url);
+const prompt = require('prompt-sync')({ sigint: true });
+
+export default class Question {
+
+  constructor(questionText) {
+    this.questionText = questionText
+    this.answer = this.askQuestion()
+  }
+
+  askQuestion() {
+    let userInput
+    do {
+      userInput = prompt(this.questionText).trim().toUpperCase()
+      if (!this.isValidResponse(userInput) == true) { 
+        console.log("Please enter letters only.")
+      }
+    } while (!this.isValidResponse(userInput) == true)
+    return userInput
+  }
+
+  isValidResponse(userInput) {
+    // Check if userInput is only letters
+    return /^[A-Ö]+$/.test(userInput) // +$ makes it possible to enter more letters; to specify how many, use: /^[A-Ö]{3}$/
+  }
+
+}
