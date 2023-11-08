b283eb8c2c71f7b9278242976db0de305a71d83e
owenboy9 <owenkraus9@gmail.com>
2023-11-08 21:10:40 +0100

    noticed i'd left .mjs in the import line

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ import SecretWord from "./secret-word.js"
 import Question from "./question.js"
 import FoundWord from "./found-word.js"
 import Gallows from "./gallows.js"
-import DonateWord from "./donate-a-word.mjs"
+import DonateWord from "./donate-a-word.js"
 import RandomWord from "./random-word.js"
 
 const print = console.log
