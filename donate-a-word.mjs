5fc7f07e05c5805ad3f3c7b27e13026351bc0ecd
owenboy9 <owenkraus9@gmail.com>
2023-11-08 14:00:58 +0100

    tidied up: incorporated both methods into the class. that is: what happens here is: adding a new word (user input) to our .csv list only IF 1. the user types nothing but letters 2. the new word is not already on the list also, following the game's already existing aesthetics, made sure user input would be all caps

--- donate-a-word.mjs
+++ donate-a-word.mjs
@@ -1,24 +1,12 @@
 import Module from "node:module"
-
 const require = Module.createRequire(import.meta.url)
-const prompt = require('prompt-sync')({ sigint: true })
-const newWord = prompt('donate a word: ')
-
-console.log(`You entered: ${newWord}`)
-
 const fs = require('fs')
 const readline = require('readline')
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-})
-
+const prompt = require('prompt-sync')({ sigint: true })
+const randomWords = fs.readFileSync("data/word-list.csv", "utf8")
 
 export default class DonateWord {
-  
   constructor() {
-    this.wordSet = new Set()
     this.rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
@@ -26,27 +14,30 @@ export default class DonateWord {
   }
 
   donateWord() {
-    this.rl.question('Donate a word: ', (newWord) => {
-      if (!this.isDuplicate(newWord)) {
-        this.wordSet.add(newWord)
-        // Append the new word to the CSV file
-        fs.appendFile('word-list.csv', newWord + '\n', () => {
-          console.log(`${newWord} was added to the hangman word list.`)
-          this.rl.close()
-        });
-      } else {
-        console.log(`${newWord} is already in the hangman word list.`)
-        this.rl.close()
+    let newWord
+    do {
+      newWord = prompt('Whether you win or lose, leave a word behind: ').trim().toUpperCase();
+      if (!this.isValidResponse(newWord)) {
+        console.log("Letters only, please.")
       }
-    })
+    } while (!this.isValidResponse(newWord))
+
+    if (!this.isDuplicate(newWord)) {
+      fs.appendFile('word-list.csv', newWord + ', ', () => {
+        console.log(`${newWord} was added to the hangman word list.`)
+        this.rl.close()
+      });
+    } else {
+      console.log(`${newWord} is already in the hangman word list. Try something else.`)
+      this.rl.close()
+    }
   }
 
   isValidResponse(newWord) {
-    return /^[A-Ö]+$/.test(newWord)
+    return /^[A-Z]+$/.test(newWord)
   }
 
   isDuplicate(newWord) {
-    return this.wordSet.has(newWord)
+    return randomWords.includes(newWord)
   }
-  
 }
