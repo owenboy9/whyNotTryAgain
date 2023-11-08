17d4dad900eecb6ead24c6696a2c3d030b205922
owenboy9 <owenkraus9@gmail.com>
2023-11-08 10:25:45 +0100

    changed class structure: now i'm trying to use Set() to keep the words in for checking new words for duplicates – chat gtp is telling me this is much faster than examining a .csv file every time. added a function that will prevent the user from typing in anything but letters it's all nothing but a sketch so far.

--- donate-a-word.mjs
+++ donate-a-word.mjs
@@ -1,11 +1,10 @@
-/*import Module from "node:module"
-const require = Module.createRequire(import.meta.url)*/
+import Module from "node:module"
 
+const require = Module.createRequire(import.meta.url)
 const prompt = require('prompt-sync')({ sigint: true })
-
 const newWord = prompt('donate a word: ')
-console.log(`You entered: ${newWord}`)
 
+console.log(`You entered: ${newWord}`)
 
 const fs = require('fs')
 const readline = require('readline')
@@ -13,20 +12,41 @@ const readline = require('readline')
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
-});
+})
 
-function getUserInput() {
-  rl.question('donate a word: ', (newWord) => {
-    // Append the new word to the CSV file
-    fs.appendFile('word-list.csv', newWord + '\n', (err) => {
-      if (err) {
-        console.error('Error writing to CSV:', err);
+
+export default class DonateWord {
+  
+  constructor() {
+    this.wordSet = new Set()
+    this.rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    })
+  }
+
+  donateWord() {
+    this.rl.question('Donate a word: ', (newWord) => {
+      if (!this.isDuplicate(newWord)) {
+        this.wordSet.add(newWord)
+        // Append the new word to the CSV file
+        fs.appendFile('word-list.csv', newWord + '\n', () => {
+          console.log(`${newWord} was added to the hangman word list.`)
+          this.rl.close()
+        });
       } else {
-        console.log(`${newWord} was added to hangman word list.`);
+        console.log(`${newWord} is already in the hangman word list.`)
+        this.rl.close()
       }
-      rl.close();
-    });
-  });
-}
+    })
+  }
 
-getUserInput()
+  isValidResponse(newWord) {
+    return /^[A-Ö]+$/.test(newWord)
+  }
+
+  isDuplicate(newWord) {
+    return this.wordSet.has(newWord)
+  }
+  
+}
